8b7eb4f5f42795ad07bc88a968002a454052193e
Ian Simpson <IDFSimpson@ymail.com>
2016-06-21 19:04:51 -0700

    add user authentication using device

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :auctions do
     resources :bids
   end
+
+  root 'auctions#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
