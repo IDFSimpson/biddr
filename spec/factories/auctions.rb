5c301c98ec0163c6fc47cab3abe1c43699659e7e
Ian Simpson <IDFSimpson@ymail.com>
2016-08-03 13:21:26 -0700

    add a seed file and helper factorygirls

--- spec/factories/auctions.rb
+++ spec/factories/auctions.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
 
     sequence(:title) {|n| "#{Faker::Commerce.product_name}-#{n}" }
     details          { Faker::Hipster.paragraph }
-    reserve_price    { rand(100000) }
+    reserve_price    { 1 + rand(100000) }
     ends             { Time.now + rand(120).days }
   end
 
@@ -13,8 +13,8 @@ FactoryGirl.define do
 
     sequence(:title) {|n| "#{Faker::Commerce.product_name}-#{n}" }
     details          { Faker::Hipster.paragraph }
-    reserve_price    { rand(1000) }
-    current_price    { rand(1000) }
+    reserve_price    { 1 + rand(1000) }
+    current_price    { 1 + rand(1000) }
     ends             { Time.now + rand(120).days }
   end
 end
